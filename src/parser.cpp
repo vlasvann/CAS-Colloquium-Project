80445f38179e3f72626816bc3637b1b101bd8d75
BabinDanil4381 <barbarian00@inbox.ru>
2025-11-04 21:22:14 +0300

    guiFiles8

--- src/parser.cpp
+++ src/parser.cpp
@@ -4,44 +4,52 @@ Parser::Parser() {}
 
 Natural Parser::parseNatural(const std::string &input)
 {
+	(void)input;
     return Natural();
 }
 
 Integer Parser::parseInteger(const std::string &input)
 {
+	(void)input;
     return Integer();
 }
 
 Rational Parser::parseRational(const std::string &input)
 {
+	(void)input;
     return Rational();
 }
 
 Polynomial Parser::parsePolynomial(const std::string &input)
 {
+	(void)input;
     return Polynomial();
 }
 
 std::string Parser::toString(const Natural &num)
 {
+	(void)num;
     std::string str = "52ngg";
     return str;
 }
 
 std::string Parser::toString(const Integer &num)
 {
+	(void)num;
     std::string str = "52ngg";
     return str;
 }
 
 std::string Parser::toString(const Rational &num)
 {
+	(void)num;
     std::string str = "52ngg";
     return str;
 }
 
 std::string Parser::toString(const Polynomial &poly)
 {
+	(void)poly;
     std::string str = "52ngg";
     return str;
 }
