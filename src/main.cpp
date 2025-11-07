7a997d01c2d817614966c77a4691bb11b81af483
Anna Vlasova <anna.vlasova000@mail.ru>
2025-11-07 21:48:58 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,8 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    app.setWindowIcon(QIcon("C:/Users/tomil/Downloads/icon1.jpg"));
     MainWindow window;
     window.show();
-
     return app.exec();
 }
+
